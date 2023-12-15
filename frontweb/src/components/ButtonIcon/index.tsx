3fece270fad89042afc661732cfb16b2904cf9ca
Vinicius Montes <viniciusmontes@outlook.com.br>
2023-12-15 13:47:18 -0300

    Finalizing the x-small home page layout

---
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,16 @@
+
+import './styles.css';
+
+
+const ButtonIcon = () => {
+    return (
+        <div className="btn-container">
+            <button className="btn btn-secundary">
+                <h1>VER CATÁLOGO</h1>
+            </button>
+        </div>
+        
+    )
+}
+
+export default ButtonIcon;
--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import Navbar from 'components/Navbar';
 import HomeImage from 'assets/images/home-image.png';
+import ButtonIcon from 'components/ButtonIcon';
 
 import './styles.css';
 
@@ -9,20 +10,24 @@ const Home = () => {
       <Navbar />
       <div className="home-container">
         <div className="home-card">
-          <div className="home-card-image-container">
-            <img src={HomeImage} alt="" />
-          </div>
-          <div className="home-card-content">
-            <h1>O carro perfeito para você</h1>
-            <p>
-              Conheça nossos carros e dê mais um passo na realização do seu
-              sonho
-            </p>
+          <div className="home-content-container">
+            <div className="home-card-image-container">
+              <img src={HomeImage} alt="" />
+            </div>
+            <div>
+              <h1>O carro perfeito para você</h1>
+              <p>
+                Conheça nossos carros e dê mais um passo na realização do seu
+                sonho
+              </p>
+            </div>
           </div>
         </div>
         <div className="home-card-button">
-          <button>VER CATÁLOGO</button>
-          <p>Comece agora a navegar</p>
+          <div className="home-card-button-content">
+            <ButtonIcon />
+            <p>Comece agora a navegar</p>
+          </div>
         </div>
       </div>
     </>

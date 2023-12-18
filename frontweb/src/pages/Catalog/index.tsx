77990d0e3079073d07ff89cb4efb968d976cf0fb
Vinicius Montes <viniciusmontes@outlook.com.br>
2023-12-17 21:37:42 -0300

    Finalizing ProductCard responsive

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -3,16 +3,28 @@ import ProductCard from 'components/ProductCard';
 
 import './styles.css';
 
-
 const Catalog = () => {
-    return (
-        <>
-        <Navbar/>
-        <div className="container my-4">
-            <ProductCard/>
+  return (
+    <>
+      <Navbar />
+      <div className="container my-4">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Catalog;
